6d11ceeef4e700b24a850def0485b0aa8b8bfd0a
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2024-03-21 20:52:37 +0900

    Update preprocess.py

--- recipes/tacotron/preprocess.py
+++ recipes/tacotron/preprocess.py
@@ -109,51 +109,48 @@ def preprocess(
             # wavファイルを読み込む
             _sr, x = wavfile.read(wav_file)
 
-    # wavファイルを読み込む
-    _sr, x = wavfile.read(wav_file)
+            # サンプルの型が int16 または int32 の場合には正規化が必要
+            if x.dtype in [np.int16, np.int32]:
+                x = (x / np.iinfo(x.dtype).max).astype(np.float64)
 
-    # サンプルの型が int16 または int32 の場合には正規化が必要
-    if x.dtype in [np.int16, np.int32]:
-        x = (x / np.iinfo(x.dtype).max).astype(np.float64)
+            x = librosa.resample(y=x, orig_sr=_sr, target_sr=sr)
+            out_feats = logmelspectrogram(x, sr)
 
-    x = librosa.resample(y=x, orig_sr=_sr, target_sr=sr)
-    out_feats = logmelspectrogram(x, sr)
+            # 冒頭と末尾の非音声区間の長さを調整
+            assert "sil" in labels.contexts[0] and "sil" in labels.contexts[-1]
+            start_frame = int(labels.start_times[1] / 125000)
+            end_frame = int(labels.end_times[-2] / 125000)
 
-    # 冒頭と末尾の非音声区間の長さを調整
-    assert "sil" in labels.contexts[0] and "sil" in labels.contexts[-1]
-    start_frame = int(labels.start_times[1] / 125000)
-    end_frame = int(labels.end_times[-2] / 125000)
+            # 冒頭： 50 ミリ秒、末尾： 100 ミリ秒
+            start_frame = max(0, start_frame - int(0.050 / 0.0125))
+            end_frame = min(len(out_feats), end_frame + int(0.100 / 0.0125))
 
-    # 冒頭： 50 ミリ秒、末尾： 100 ミリ秒
-    start_frame = max(0, start_frame - int(0.050 / 0.0125))
-    end_frame = min(len(out_feats), end_frame + int(0.100 / 0.0125))
+            out_feats = out_feats[start_frame:end_frame]
 
-    out_feats = out_feats[start_frame:end_frame]
+            # 時間領域で音声の長さを調整
+            x = x[int(start_frame * 0.0125 * sr) :]
+            length = int(sr * 0.0125) * out_feats.shape[0]
+            x = pad_1d(x, length) if len(x) < length else x[:length]
 
-    # 時間領域で音声の長さを調整
-    x = x[int(start_frame * 0.0125 * sr) :]
-    length = int(sr * 0.0125) * out_feats.shape[0]
-    x = pad_1d(x, length) if len(x) < length else x[:length]
+            # 特徴量のアップサンプリングを行う都合上、音声波形の長さはフレームシフトで割り切れる必要があります
+            assert len(x) % int(sr * 0.0125) == 0
 
-    # 特徴量のアップサンプリングを行う都合上、音声波形の長さはフレームシフトで割り切れる必要があります
-    assert len(x) % int(sr * 0.0125) == 0
+            # mu-law量子化
+            x = mulaw_quantize(x, mu)
 
-    # mu-law量子化
-    x = mulaw_quantize(x, mu)
-
-    # save to files
-    utt_id = lab_file.stem
-    np.save(in_dir / f"{utt_id}-feats.npy", in_feats, allow_pickle=False)
-    np.save(
-        out_dir / f"{utt_id}-feats.npy",
-        out_feats.astype(np.float32),
-        allow_pickle=False,
-    )
-    np.save(
-        wave_dir / f"{utt_id}-feats.npy",
-        x.astype(np.int64),
-        allow_pickle=False,
-    )
+            # save to files
+            utt_id = lab_file.stem
+            np.save(in_dir / f"{utt_id}-feats.npy", in_feats, allow_pickle=False)
+            np.save(
+                out_dir / f"{utt_id}-feats.npy",
+                out_feats.astype(np.float32),
+                allow_pickle=False,
+            )
+            np.save(
+                wave_dir / f"{utt_id}-feats.npy",
+                x.astype(np.int64),
+                allow_pickle=False,
+            )
 
 
 if __name__ == "__main__":
