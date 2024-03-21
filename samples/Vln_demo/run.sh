8064e8fd36b4556609a7dd16b2dd38ac9d7b9db4
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2024-03-21 13:47:57 +0900

    Update run.sh

--- samples/Vln_demo/run.sh
+++ samples/Vln_demo/run.sh
@@ -70,9 +70,9 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
 fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
-    echo "stage 1: Tacotronのための特徴量生成"
+    echo "stage 1: Feature generation for Tacotron"
     for s in ${datasets[@]}; do
-        xrun python preprocess.py data/$s.list /content/recipes/tacotron/downloads/ $lab_root \
+        xrun python preprocess.py data/$s.list $wav_root $lab_root \
             $dump_org_dir/$s --n_jobs $n_jobs \
             --sample_rate $sample_rate --mu $mu
     done
