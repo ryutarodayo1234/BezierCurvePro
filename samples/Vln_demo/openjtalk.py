05e621cec36b96c33bac0c421a62b96de399fff5
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2024-03-25 20:55:53 +0900

    Update openjtalk.py

--- samples/Vln_demo/openjtalk.py
+++ samples/Vln_demo/openjtalk.py
@@ -63,7 +63,6 @@ _pad = "~"
 # NOTE: 0 をパディングを表す数値とする
 symbols = [_pad] + extra_symbols + phonemes
 
-
 _symbol_to_id = {s: i for i, s in enumerate(symbols)}
 _id_to_symbol = {i: s for i, s in enumerate(symbols)}
 
@@ -77,27 +76,34 @@ def numeric_feature_by_regex(regex, s):
 additional_symbols2 = [
     '_',
 
-    'C1', 'C-1', 'D1', 'D-1', 'E1', 'F1', 'F-1', 'G1', 'G-1', 'A1', 'A-1', 'B1',
-    'C2', 'C-2', 'D2', 'D-2', 'E2', 'F2', 'F-2', 'G2', 'G-2', 'A2', 'A-2', 'B2',
-    'C3', 'C-3', 'D3', 'D-3', 'E3', 'F3', 'F-3', 'G3', 'G-3', 'A3', 'A-3', 'B3',
-    'C4', 'C-4', 'D4', 'D-4', 'E4', 'F4', 'F-4', 'G4', 'G-4', 'A4', 'A-4', 'B4',
-    'C5', 'C-5', 'D5', 'D-5', 'E5', 'F5', 'F-5', 'G5', 'G-5', 'A5', 'A-5', 'B5',
-    'C6', 'C-6', 'D6', 'D-6', 'E6', 'F6', 'F-6', 'G6', 'G-6', 'A6', 'A-6', 'B6',
+    'C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1', 'A1', 'A#1', 'B1',
+    'C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2',
+    'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3',
+    'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4',
+    'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5',
+    'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6', 'A6', 'A#6', 'B6',
+
+    'C1', 'D-1', 'E-1', 'F1', 'G-1', 'A-1', '--1', '-1',
+    'D-2', 'E-2', 'F2', 'G-2', 'A-2', '--2', '-2',
+    'C3', 'D-3', 'E-3', 'F3', 'G-3', 'A-3', '--3', '-3',
+    'D-4', 'E-4', 'F4', 'G-4', 'A-4', '--4', '-4',
+    'C5', 'D-5', 'E-5', 'F5', 'G-5', 'A-5', '--5', '-5',
+    'D-6', 'E-6', 'F6', 'G-6', 'A-6', '--6', '-6'
 
     'whole', 'half', 'quarter', 'eighth', '16th', '32nd', '64th', '128th',
 
     'common', 'cut', '2/2', '2/4', '3/2', '3/4', '4/2', '4/4', '5/2', '5/4', '6/2', '6/4', '7/2', '7/4', '8/2', '8/4', '9/2', '9/4', '12/2', '12/4',
 
-    'C major', 'G major', 'D major', 'A major', 'E major', 'B major', 'F- major', 'C- major',
+    'C major', 'G major', 'D major', 'A major', 'E major', 'B major', 'F# major', 'C# major',
     'F major', 'Bb major', 'Eb major', 'Ab major', 'Db major', 'Gb major', 'Cb major',
-    'a minor', 'e minor', 'b minor', 'f- minor', 'c- minor', 'g- minor', 'd- minor', 'a- minor',
+    'a minor', 'e minor', 'b minor', 'f# minor', 'c# minor', 'g# minor', 'd# minor', 'a# minor',
     'd minor', 'g minor', 'c minor', 'f minor', 'bb minor', 'eb minor', 'ab minor',
-    'C lydian', 'G lydian', 'D lydian', 'A lydian', 'E lydian', 'B lydian', 'F- lydian', 'C- lydian',
-    'C mixolydian', 'G mixolydian', 'D mixolydian', 'A mixolydian', 'E mixolydian', 'B mixolydian', 'F- mixolydian', 'C- mixolydian',
-    'C dorian', 'G dorian', 'D dorian', 'A dorian', 'E dorian', 'B dorian', 'F- dorian', 'C- dorian',
-    'C phrygian', 'G phrygian', 'D phrygian', 'A phrygian', 'E phrygian', 'B phrygian', 'F- phrygian', 'C- phrygian',
-    'C aeolian', 'G aeolian', 'D aeolian', 'A aeolian', 'E aeolian', 'B aeolian', 'F- aeolian', 'C- aeolian',
-    'C locrian', 'G locrian', 'D locrian', 'A locrian', 'E locrian', 'B locrian', 'F- locrian', 'C- locrian',
+    'C lydian', 'G lydian', 'D lydian', 'A lydian', 'E lydian', 'B lydian', 'F# lydian', 'C# lydian',
+    'C mixolydian', 'G mixolydian', 'D mixolydian', 'A mixolydian', 'E mixolydian', 'B mixolydian', 'F# mixolydian', 'C# mixolydian',
+    'C dorian', 'G dorian', 'D dorian', 'A dorian', 'E dorian', 'B dorian', 'F# dorian', 'C# dorian',
+    'C phrygian', 'G phrygian', 'D phrygian', 'A phrygian', 'E phrygian', 'B phrygian', 'F# phrygian', 'C# phrygian',
+    'C aeolian', 'G aeolian', 'D aeolian', 'A aeolian', 'E aeolian', 'B aeolian', 'F# aeolian', 'C# aeolian',
+    'C locrian', 'G locrian', 'D locrian', 'A locrian', 'E locrian', 'B locrian', 'F# locrian', 'C# locrian',
 
     'ppp', 'pp', 'p', 'mp', 'mf', 'f', 'ff', 'fff', 'sffz',
 
