eabe7e52724171149eb0d3903f30723a5c8ecbe4
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2024-03-25 20:33:19 +0900

    add additional_symbols2

--- samples/Vln_demo/openjtalk.py
+++ samples/Vln_demo/openjtalk.py
@@ -126,11 +126,61 @@ def numeric_feature_by_regex(regex, s):
         return -50
     return int(match.group(1))
 
+additional_symbols2 = [
+    '_',
+
+    'C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1', 'A1', 'A#1', 'B1',
+    'C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2',
+    'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3',
+    'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4',
+    'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5',
+    'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6', 'A6', 'A#6', 'B6',
+
+    'whole', 'half', 'quarter', 'eighth', '16th', '32nd', '64th', '128th',
+
+    'common', 'cut', '2/2', '2/4', '3/2', '3/4', '4/2', '4/4', '5/2', '5/4', '6/2', '6/4', '7/2', '7/4', '8/2', '8/4', '9/2', '9/4', '12/2', '12/4',
+
+    'C major', 'G major', 'D major', 'A major', 'E major', 'B major', 'F# major', 'C# major',
+    'F major', 'Bb major', 'Eb major', 'Ab major', 'Db major', 'Gb major', 'Cb major',
+    'a minor', 'e minor', 'b minor', 'f# minor', 'c# minor', 'g# minor', 'd# minor', 'a# minor',
+    'd minor', 'g minor', 'c minor', 'f minor', 'bb minor', 'eb minor', 'ab minor',
+    'C lydian', 'G lydian', 'D lydian', 'A lydian', 'E lydian', 'B lydian', 'F# lydian', 'C# lydian',
+    'C mixolydian', 'G mixolydian', 'D mixolydian', 'A mixolydian', 'E mixolydian', 'B mixolydian', 'F# mixolydian', 'C# mixolydian',
+    'C dorian', 'G dorian', 'D dorian', 'A dorian', 'E dorian', 'B dorian', 'F# dorian', 'C# dorian',
+    'C phrygian', 'G phrygian', 'D phrygian', 'A phrygian', 'E phrygian', 'B phrygian', 'F# phrygian', 'C# phrygian',
+    'C aeolian', 'G aeolian', 'D aeolian', 'A aeolian', 'E aeolian', 'B aeolian', 'F# aeolian', 'C# aeolian',
+    'C locrian', 'G locrian', 'D locrian', 'A locrian', 'E locrian', 'B locrian', 'F# locrian', 'C# locrian',
+
+    'ppp', 'pp', 'p', 'mp', 'mf', 'f', 'ff', 'fff', 'sffz',
+
+    'grave', 'larghissimo', 'larghetto', 'largo', 'lento', 'adagio', 'andante', 'moderato', 'allegretto', 'allegro', 'vivace', 'presto', 'prestissimo',
+
+    'staccato', 'tenuto', 'marcato', 'accent', 'portato', 'legato',
+
+    'segno', 'coda', 'D.C.', 'D.S.', 'fine', 'To Coda', 'To Fine', 'To Segno', 'Da Capo', 'Dal Segno', 'Dal Segno al Coda', 'Da Capo al Fine', 'Da Capo al Segno', 'Da Capo al Coda',
+
+    'fermata', 'fermata square', 'fermata triangle', 'fermata circle',
+
+    'adagio', 'andante', 'moderato', 'allegretto', 'allegro', 'vivace', 'presto', 'prestissimo',
+    'ritardando', 'rit.', 'accelerando', 'accel.', 'rallentando', 'rall.', 'stringendo', 'stretto', 'allargando', 'a tempo',
+
+    'crescendo', 'cresc.', 'diminuendo', 'dim.', 'decrescendo', 'decresc.', 'crescendo diminuendo', 'crescendo-diminuendo', 'decrescendo crescendo', 'decrescendo-crescendo', 'hairpin', 'hairpin crescendo', 'hairpin diminuendo', 'hairpin decrescendo',
+
+    '8va', '8vb', '15ma', '15mb', 'loco',
+
+    'tremolo', 'trem.', 'tremolando', 'tremol.', 'tr.', 'trill',
+
+    'accent', 'marcato', 'sforzando', 'sfz', 'strong accent', 'heavy accent', 'stress accent',
+
+    'text', 'lyric', 'tempo', 'direction', 'annotation', 'rehearsal mark', 'dynamic mark', 'tempo mark', 'expression', 'articulation',
+    'segno', 'coda', 'D.C.', 'D.S.', 'fine', 'To Coda', 'To Fine', 'To Segno', 'Da Capo', 'Dal Segno', 'Dal Segno al Coda', 'Da Capo al Fine', 'Da Capo al Segno', 'Da Capo al Coda',
+]
 
 def pp_symbols(labels, additional_symbols=None):
     # OpenJTalkラベルから韻律記号付き音素列を抽出する
     if additional_symbols:
         symbols = "|".join([r"\b" + re.escape(s) + r"\b" for s in additional_symbols])
+        symbols += additional_symbols2
         symbols_pattern = f"({symbols})"
         symbols_pattern = re.compile(symbols_pattern)
     else:
