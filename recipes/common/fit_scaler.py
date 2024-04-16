5dfcb9ef060b6d6223e60fd80cdc6c6c32166c5d
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2024-04-16 10:28:34 +0900

    Update fit_scaler.py

--- recipes/common/fit_scaler.py
+++ recipes/common/fit_scaler.py
@@ -4,9 +4,8 @@ from pathlib import Path
 
 import joblib
 import numpy as np
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 from tqdm import tqdm
-
 
 def get_parser():
     parser = argparse.ArgumentParser(description="Fit scalers")
@@ -16,14 +15,13 @@ def get_parser():
     parser.add_argument("--external_scaler", type=str, help="External scaler")
     return parser
 
-
 if __name__ == "__main__":
     args = get_parser().parse_args(sys.argv[1:])
     in_dir = Path(args.in_dir)
     if args.external_scaler is not None:
         scaler = joblib.load(args.external_scaler)
     else:
-        scaler = StandardScaler()
+        scaler = MinMaxScaler()  # MinMaxScalerを使用する
     with open(args.utt_list) as f:
         for utt_id in tqdm(f):
             c = np.load(in_dir / f"{utt_id.strip()}-feats.npy")
