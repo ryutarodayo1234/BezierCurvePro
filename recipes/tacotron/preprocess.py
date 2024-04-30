b6bbbbe5e99ec191f40af706b5c2ecda1900b0b3
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2024-04-30 10:59:43 +0900

    Update preprocess.py

--- recipes/tacotron/preprocess.py
+++ recipes/tacotron/preprocess.py
@@ -91,7 +91,7 @@ def preprocess(
     # 特徴量を格納するリスト
     features = []
     
-    '''
+    
     # 各行のラベル情報から特徴量を計算
     for line in labels:
         # 各行をタブで分割して情報を取得
@@ -105,21 +105,30 @@ def preprocess(
         duration = end_time - start_time
         # ピッチ（音高）を数値に変換
         pitch_value = pitch_to_number(pitch)
-        # 特徴量として開始時間、終了時間、ピッチ、音符の長さを追加
-        features.append([start_time, end_time, pitch_value, duration])
+        # 特徴量として開始時間、ピッチ、音符の長さを追加
+        # *****終了時間、end_time,を削除、音符の長さがあればいらない？*****
+        features.append([start_time, pitch_value, duration])
 
         # 特徴量のリストをNumPy配列に変換
         in_feats = np.array(features, dtype=np.float32)
-    '''
-    
-    assert wav_file.stem == lab_file.stem
-    labels = hts.load(lab_file)
-    # 韻律記号付き音素列の抽出
-    PP = pp_symbols(labels.contexts)
-    in_feats = np.array(text_to_sequence(PP), dtype=np.int64)
 
-    # メルスペクトログラムの計算
+        # wavファイルを読み込む
     _sr, x = wavfile.read(wav_file)
+
+    # デバッグ用: トリミング前の波形の長さを出力
+    print("Before trimming - Length of waveform:", len(x))
+
+    # 波形の長さが条件を満たすようにトリミングする
+    required_length = int(sr * 0.0125 * np.floor(len(x) / (sr * 0.0125)))
+    trimmed_x = x[-required_length:]  # 後ろからトリミングする
+
+    # デバッグ用: トリミング後の波形の長さを出力
+    print("After trimming - Length of waveform:", len(trimmed_x))
+
+    # 元のファイルにトリミングされた波形を上書きする
+    wavfile.write(wav_file, sr, trimmed_x)
+    
+    # メルスペクトログラムの計算
     if x.dtype in [np.int16, np.int32]:
         x = (x / np.iinfo(x.dtype).max).astype(np.float64)
     x = librosa.resample(y=x, orig_sr=_sr, target_sr=sr)
@@ -130,22 +139,10 @@ def preprocess(
     start_frame = int(labels.start_times[1] / 125000)
     end_frame = int(labels.end_times[-2] / 125000)
 
-    # 冒頭： 50 ミリ秒、末尾： 100 ミリ秒
-    start_frame = max(0, start_frame - int(0.050 / 0.0125))
-    end_frame = min(len(out_feats), end_frame + int(0.100 / 0.0125))
-    out_feats = out_feats[start_frame:end_frame]
-
-    # 時間領域で音声の長さを調整
-    x = x[int(start_frame * 0.0125 * sr) :]
-    length = int(sr * 0.0125) * out_feats.shape[0]
-    x = pad_1d(x, length) if len(x) < length else x[:length]
-
     # 特徴量のアップサンプリングを行う都合上、音声波形の長さはフレームシフトで割り切れる必要があります
     assert len(x) % int(sr * 0.0125) == 0
-
     # mu-law量子化
     x = mulaw_quantize(x, mu)
-    
 
     # save to files
     utt_id = os.path.basename(lab_file).split('.')[0]  # 拡張子を除いたファイル名
