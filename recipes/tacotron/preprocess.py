7110bfc42a2a6cce78e727ec405cb244e7c6c2dc
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2024-04-30 13:16:37 +0900

    Update preprocess.py

--- recipes/tacotron/preprocess.py
+++ recipes/tacotron/preprocess.py
@@ -134,13 +134,9 @@ def preprocess(
     x = librosa.resample(y=x, orig_sr=_sr, target_sr=sr)
     out_feats = logmelspectrogram(x, sr)
 
-    # 冒頭と末尾の非音声区間の長さを調整
-    assert "sil" in labels.contexts[0] and "sil" in labels.contexts[-1]
-    start_frame = int(labels.start_times[1] / 125000)
-    end_frame = int(labels.end_times[-2] / 125000)
-
     # 特徴量のアップサンプリングを行う都合上、音声波形の長さはフレームシフトで割り切れる必要があります
     assert len(x) % int(sr * 0.0125) == 0
+    
     # mu-law量子化
     x = mulaw_quantize(x, mu)
 
